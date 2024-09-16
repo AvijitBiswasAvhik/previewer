5ba34e85a36b6495dbb0a331d4c8bf2d7cdb3c53
AvijitBiswasAvhik <avijitbiswasavhik@gmail.com>
2024-09-16 22:31:44 +0600

    hello

--- src/App.js
+++ src/App.js
@@ -4,35 +4,44 @@ import "./App.css";
 import Previewer from "./previewer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import markdownText from "./txt";
+
 
 function App() {
-  let [data, setData] = useState();
+  let [data, setData] = useState(markdownText);
+
   function edtorFullArea(e) {
     e.stopPropagation();
-    let txtArea = document.getElementById('editor');
-    let preview = document.getElementById('preview');
-    if (txtArea.classList.contains('editor')) {
-      txtArea.classList.remove('editor');
-      preview.classList.remove('off')
-    }else if(!txtArea.classList.contains('editor')){
-      txtArea.classList.add('editor');
-      preview.classList.add('off')
+    let txtArea = document.getElementById("editor");
+    let preview = document.getElementById("preview");
+    if (txtArea.classList.contains("editor")) {
+      txtArea.classList.remove("editor");
+      preview.classList.remove("off");
+    } else if (!txtArea.classList.contains("editor")) {
+      txtArea.classList.add("editor");
+      preview.classList.add("off");
     }
   }
   return (
     <div className="App ">
-      <div className="text-editor-box shadow rounded overflow-hidden mt-2" id="editor">
+      <div
+        className="text-editor-box shadow rounded overflow-hidden mt-2"
+        id="editor"
+      >
         <div className="text-editor-header d-flex align-item-center justify-content-between border-0 ">
           <i className="fa-brands fa-free-code-camp"> Editor</i>
-          <i className="fa fa-arrows-alt" onClick={(e)=>edtorFullArea(e)}></i>
+          <i className="fa fa-arrows-alt" onClick={(e) => edtorFullArea(e)}></i>
         </div>
         <textarea
           className=" bg-info-subtle"
-          onChange={(e)=>{
+          onChange={(e) => {
             e.stopPropagation();
-            setData(e.target.value)
+            setData(e.target.value);
           }}
-        ></textarea>
+          value={data}
+        >
+         
+        </textarea>
       </div>
       <Previewer data={data} />
     </div>
